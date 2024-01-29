a4e84d5bc07248dff01fd6f0d9c9da05c02d5d41
Victor Gomes <46842347+victorgomesl@users.noreply.github.com>
2024-01-29 17:35:53 -0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [
-  {
-    path:"home",
-    component: HomeComponent,
-  },
-  {
-    path:'**',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  }
-];
+
+export const routes: Routes = [];
