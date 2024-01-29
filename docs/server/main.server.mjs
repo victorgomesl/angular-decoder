ea964699919d251fe5763e88c6b474ca8c179d3a
Victor Gomes <46842347+victorgomesl@users.noreply.github.com>
2024-01-29 17:13:12 -0300

    Github pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2TRHQZHB.mjs";import"./chunk-WVQKPOHX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IMFFIDZF.mjs";import"./chunk-WVQKPOHX.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
